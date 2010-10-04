6070ab9263cce501cd520ac43b0367f230a3c870
lmiropol <lmiropol@hutlab3.sph.harvard.edu>
2010-10-04 16:51:07 -0400

    General clean up, using realtive path instead full

--- ArrayExpress/subdirectories.rb
+++ ArrayExpress/subdirectories.rb
@@ -6,9 +6,9 @@ STDIN.each do |strID|
 		next; end
 	strID.strip!
 	system( "mkdir -p " + strID )
-	strTo = strID + "/Makefile"
-	if( !File.exist?( strTo ) )
-		system( "cp Makefile.template " + strTo ); end; end
+	#strTo = strID + "/Makefile"
+	#if( !File.exist?( strTo ) )
+	#	system( "cp Makefile.template " + strTo ); end; end
 	#strTo2 = strID + "/SConscript"
 	#if( !File.exist?( strTo2 ) )
 	#	system( "cp SConscript.common " + strTo2);  end; end
