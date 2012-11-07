06c3c35f47b9d55cb20d767a1e62594ad4c66da8
Joseph Moon <moon@college.harvard.edu>
2012-11-07 18:06:47 -0500

    Safeguard commit after Daniela/Joseph code review

--- STRING/src/SConscript_1-id.py
+++ STRING/src/SConscript_1-id.py
@@ -13,20 +13,19 @@ def test( iLevel, strID, hashArgs ):
 if locals( ).has_key( "testing" ):
 	sys.exit( )
 
-
 c_strID					= arepa.cwd( )
-c_Taxa                  = c_strID.split("_")[1]
+c_strTaxa                  		= c_strID.split("_")[1]
 
 
-c_fileInputC    		= File( sfle.d( arepa.path_repo( ), sfle.c_strDirTmp, "stringc" ) )
+c_fileInputC    			= sfle.d( pE, arepa.path_repo( ), sfle.c_strDirTmp, "stringc" ) 
 
-c_fileIDPKL				= File( c_strID + ".pkl" )
-c_fileIDDAB				= File( c_strID + ".dab" )
-c_fileIDDAT             = File( c_strID + ".dat" )
-c_fileIDQUANT           = File( c_strID + ".quant" )
+c_fileIDPKL				= sfle.d( pE, c_strID + ".pkl" )
+c_fileIDDAB				= sfle.d( pE, c_strID + ".dab" )
+c_fileIDDAT             		= sfle.d( pE, c_strID + ".dat" )
+c_fileIDQUANT          			= sfle.d( pE,  c_strID + ".quant" )
 
-c_fileProgC2Metadata	= File( sfle.d( arepa.path_repo( ), sfle.c_strDirSrc, "c2metadata.py" ) )
-c_fileProgC2DAT			= File( sfle.d( arepa.path_repo( ), sfle.c_strDirSrc, "c2dat.py" ) )
+c_fileProgC2Metadata			= sfle.d( pE, arepa.path_repo( ), sfle.c_strDirSrc, "c2metadata.py" ) 
+c_fileProgC2DAT				= sfle.d( pE, arepa.path_repo( ), sfle.c_strDirSrc, "c2dat.py" ) 
 
 
 #For GeneMapper:
@@ -34,28 +33,21 @@ c_fileIDMapDAT      =  c_strID + "_mapped.dat"
 c_fileIDMapDAB      =  c_strID + "_mapped.dab"
 c_fileIDMapQUANT    =  c_strID + "_mapped.quant"
 c_strGeneTo         = sfle.readcomment( sfle.d( arepa.path_arepa(),sfle.c_strDirEtc,"geneid" ) or ["Ck"] )
-c_path_GeneMapper   =  sfle.d( arepa.path_arepa(), "GeneMapper")
-c_funcGeneMapper    =  sfle.d( c_path_GeneMapper, sfle.c_strDirSrc, "bridgemapper.py" )
 c_path_Mappingfiles =  sfle.d( arepa.path_arepa( ), "GeneMapper", sfle.c_strDirEtc, "uniprotko")
 c_fileMappingfileUniprot2KO = sfle.d(c_path_Mappingfiles, "mappingfile_allspecies_uniref2KO.map")
 c_fileMappingHuman  =  sfle.d( c_path_GeneMapper, sfle.c_strDirEtc,"Hs_Derby_20110601.bridge")
-c_funcChildrenTaxa  =  sfle.d( c_path_GeneMapper, sfle.c_strDirSrc, "getTaxidsFromChildren.py" )
-c_filetaxachildren  =  sfle.d( arepa.path_repo( ), sfle.c_strDirTmp,"taxachildren.txt")
 
-c_strStatus	    = "status.txt"
-
+c_fileStatus	    = sfle.d( pE, "status.txt" )
 
 pE = DefaultEnvironment( )
 
-#afileIDTXT = sfle.pipe( pE, c_fileInputC, c_fileProgC2Metadata, c_fileIDPKL,[[False, c_strID]] )
-#Default( afileIDTXT )
+afileIDTXT = sfle.pipe( pE, c_fileInputC, c_fileProgC2Metadata, c_fileIDPKL,[[c_strID]] )
 
 def funcDAB( target, source, env ):
 	strT, astrSs = sfle.ts( target, source )
 	strProg, strIn = astrSs[:2]
 	return sfle.ex( (sfle.cat( strIn ), "|", strProg, c_strID, "| Dat2Dab -o", strT) )
 Command( c_fileIDDAB, [c_fileProgC2DAT, c_fileInputC], funcDAB )
-
 
 def funcDAT(target, source, env):
     strT, astrSs = sfle.ts( target, source )
@@ -70,82 +62,10 @@ def funcIDQUANT( target, source, env ):
 Command( c_fileIDQUANT, c_fileIDDAB ,funcIDQUANT )
 Default (c_fileIDQUANT)
 
-
+#use shared code for dat and dab 
 
 ##############################################
 #- Gene id mapping from Uniprot to Genesymbols
 ##############################################
 
-#- Get Mappingfile
-def func_GetMappingfileFromDir(taxid):
-    # all mappingfiles from the directory:
-    files = [os.path.basename(p) for p in glob.glob(sfle.d(c_path_Mappingfiles, "*.map"))]
-    # our mappingfile that starts with the taxid:
-    f = [mm.startswith(taxid+"_") for mm in files]
-    try:
-        ix = f.index(True)
-        return files[ix]
-    except ValueError:
-        return "X"
-
-
-def func_GetMappingfile(taxid):
-    if taxid == "9606":
-        return c_fileMappingHuman
-    else:
-        return "mapping_taxid"+str(c_Taxa)+".txt"
-
-
-
-#- Do the Mapping:
-def funcGeneIdMapping( target, source, env):
-    strT, astrSs = sfle.ts( target, source )
-#    strFunc, strDATin, strPKL = astrSs[:3] #TODO: we don't have metadata for string!
-    strFunc, strDATin = astrSs[:2]
-#    pMetadata = metadata.open(open(strPKL,"rb") )
-#    c_Taxa = pMetadata["taxid"]
-    sys.stderr.write("+++ GENE ID Mapping +++ \n")
-    ## START DETERMINE MAPPINGFILE
-    c_mappingfilename = func_GetMappingfileFromDir(c_Taxa) #Normal mapping in Human: func_GetMappingfile(c_Taxa)
-    c_mappingfile = sfle.d(c_path_Mappingfiles, c_mappingfilename)
-    ## END DETERMINE MAPPINGFILE
-    ## START Take general Uniprot to KO mappingfile if no mappingfile exists so far...
-    if not os.path.exists(c_mappingfile):
-        sys.stderr.write("+++ No species-specific mappingfile exists, take the general mappingfile from uniprot.  +++ \n")
-        c_mappingfile = c_fileMappingfileUniprot2KO
-    ## END Take general Uniprot to KO mappingfile if no mappingfile exists so far...
-    return sfle.ex([ strFunc, strDATin, strT, "-m", c_mappingfile, "-c", "[0,1]", "-f", "Kg", "-t",c_strGeneTo[0], "-l", c_strStatus])
-#Command(c_fileIDMapDAT, [c_funcGeneMapper, c_fileIDDAT, c_fileIDPKL], funcGeneIdMapping)
-Command(c_fileIDMapDAT, [c_funcGeneMapper, c_fileIDDAT], funcGeneIdMapping)
-
-
-def funcGeneIdMapping2( target, source, env):
-    strT, astrSs = sfle.ts( target, source )
-    strFunc, strDATin = astrSs[:2]
-    sys.stderr.write("+++ GENE ID Mapping +++ \n")
-    return sfle.ex([ strFunc, strDATin, strT, c_fileMappingHuman,"[0,1]", "S", c_strGeneTo[0],"None"])
-#Command(c_fileIDMapDAT, [c_funcGeneMapper, c_fileIDDAT], funcGeneIdMapping2)
-
-def funcDABmapped( target, source, env ):
-    strT, astrSs = sfle.ts( target, source )
-    fileOut,fileMap = astrSs[:2]
-    if os.stat(fileMap)[6]!=0:
-        return sfle.ex([" Dat2Dab -o", strT, "-i", fileMap])
-    else:
-        return sfle.ex(["touch",strT])
-Command( c_fileIDMapDAB, [c_fileIDDAT,c_fileIDMapDAT], funcDABmapped )
-Default(c_fileIDMapDAB)
-
-
-def funcIDQUANTMapped( target, source, env ):
-    strT, astrSs = sfle.ts( target, source )
-    fileOut,fileMap = astrSs[:2]
-    if os.stat(fileMap)[6]!=0:
-        return (sfle.ex("echo '0.5\t1.5' >" + strT))
-    else:
-        return sfle.ex(["touch",strT])
-Command( c_fileIDMapQUANT, [c_fileIDDAT,c_fileIDMapDAT] ,funcIDQUANTMapped )
-Default ( c_fileIDMapQUANT)
-
-
-
+#use shared code 
--- src/SConscript_genemapping.py
+++ src/SConscript_genemapping.py
@@ -13,36 +13,80 @@ import itertools
 
 c_strID 		= arepa.cwd( )
 
-c_astrGeneTo             = sfle.readcomment( sfle.d( arepa.path_arepa(),sfle.c_strDirEtc,"geneid" ) or ["H"] ) 
+c_strMapped		= "_mapped" 
+c_astrGeneTo            = sfle.readcomment( sfle.d( arepa.path_arepa(),sfle.c_strDirEtc,"geneid" ) or ["H"] ) 
 c_piCounter		=  itertools.count(0) 
 
 c_path_GeneMapper       = sfle.d( arepa.path_arepa(), "GeneMapper")
 c_fileProgMakeUnique    = sfle.d( arepa.path_arepa(),sfle.c_strDirSrc,"makeunique.py")
 c_funcGeneMapper        = sfle.d( c_path_GeneMapper, sfle.c_strDirSrc, "bridgemapper.py" )
 
-pE = DefaultEnvironment( )
-
 def funcCounter( pCount ):
 	iTemp = next(pCount)
 	iOut = None
 	if iTemp < 10:
-		iOut="_mapped0"+str(iTemp)
+		iOut= "0"+str(iTemp)
 	else:
-		iOut="_mapped"+str(iTemp)
+		iOut= str(iTemp)
 	return iOut  
 
-def funcGeneIdMapping( strDATin, strMAPin, strLOGout ):
-	strBase, strExt = os.path.splitext(strDATin)
+def funcGeneIDMapping( pE, strDATin, strMAPin, strLOGout ):
+	strBase, strExt = os.path.splitext(str(strDATin))
 	iCount = funcCounter(c_piCounter)
     	strCOL = None 
     	if strExt == ".dat":
         	strCOL = "[0,1]"
     	elif strEXT == ".pcl":
         	strCOL = "[0]" 
-    	return sfle.op( pE, c_funcGeneMapper, [strDATin, strBase+iCount+strExt,"-m",strMAPin,"-c",strCOL,"-f", c_strGeneFrom,"-t",c_astrGeneTo[0],"-l",strLOGout] ) 
+	strT = strBase+c_strMapped+iCount+strExt
+    	return sfle.op( pE, c_funcGeneMapper, [[strDATin], [True,strT],"-m",[strMAPin],"-c",strCOL,"-f", c_strGeneFrom,"-t",c_astrGeneTo[0],"-l",[True, strLOGout]] ) 
 
-def funcMakeUnique( strDATin ):
-	strBase, strExt = os.path.splitext(strDATin)
+def funcMakeUnique( pE, strDATin ):
+	strBase, strExt = os.path.splitext(str(strDATin))
 	iCount = funcCounter(c_piCounter)
-    	return sfle.op( pE, c_fileProgMakeUnique, [strDATin, strBase+iCount+strExt] )
+	strT = strBase[:-2]+iCount+strExt
+    	return sfle.op(pE, c_fileProgMakeUnique, [[strDATin], [True,strT]])
+
+
+'''
+
+this code should be shared across all the modules 
+
+#- Get Mappingfile
+def func_GetMappingfileFromDir(taxid):
+    # all mappingfiles from the directory:
+    files = [os.path.basename(p) for p in glob.glob(sfle.d(c_path_Mappingfiles, "*.map"))]
+    # our mappingfile that starts with the taxid:
+    f = [mm.startswith(taxid+"_") for mm in files]
+    try:
+        ix = f.index(True)
+        return files[ix]
+    except ValueError:
+        return "X"
+
+def func_GetMappingfile(taxid):
+    if taxid == "9606":
+        return c_fileMappingHuman
+    else:
+        return "mapping_taxid"+str(c_Taxa)+".txt"
+
+#- Do the Mapping:
+def funcGeneIdMapping( target, source, env):
+    astrTs, astrSs = ([f.get_abspath() for f in a] for a in (target,source))
+    strT, strStatus = astrTs[:2]
+    strFunc, strDATin, strPKL = astrSs[:3]
+    pMetadata = metadata.open(open(strPKL,"rb") )
+    c_Taxa = pMetadata["taxid"]
+    sys.stderr.write("+++ GENE ID Mapping +++ \n")
+    ## START DETERMINE MAPPINGFILE
+    c_mappingfilename = func_GetMappingfileFromDir(c_Taxa) #Normal mapping in Human: func_GetMappingfile(c_Taxa)
+    c_mappingfile = sfle.d(c_path_Mappingfiles, c_mappingfilename)
+    if not os.path.exists(c_mappingfile):
+        sys.stderr.write("+++ No species-specific mappingfile exists, take the general mappingfile from uniprot.  +++ \n")
+        c_mappingfile = c_fileMappingfileUniprot2KO
+        ## END Take general Uniprot to KO mappingfile if no mappingfile exists so far...
+    return sfle.ex([ strFunc, strDATin, strT, "-m", c_mappingfile,"-c", "[0,1]", "-f", c_stringBiogridGeneID, "-t",c_strGeneTo[0], "-l", strStatus])
+Command([c_fileIDMapDAT,c_fileStatus],[c_funcGeneMapper, c_fileIDDAT, c_fileIDPKL], funcGeneIdMapping)
+'''
+
 
